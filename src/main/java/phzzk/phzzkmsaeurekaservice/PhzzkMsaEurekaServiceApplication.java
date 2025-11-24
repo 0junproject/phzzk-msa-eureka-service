56d09f79db3c8b0840946758942ab9cccd57a3c4
0yjunproject <0yjun.project@gmail.com>
2025-11-24 22:56:32 +0900

    eureka commit

---
+++ src/main/java/phzzk/phzzkmsaeurekaservice/PhzzkMsaEurekaServiceApplication.java
@@ -0,0 +1,13 @@
+package phzzk.phzzkmsaeurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhzzkMsaEurekaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhzzkMsaEurekaServiceApplication.class, args);
+    }
+
+}
